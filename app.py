dbe81340afdef4f96604cfa9ee043342f2014b6d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-07-10 23:58:25 +0200

    Add loop to find all ACTORS nodes in static.xml

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('static.xml')
 root = tree.getroot()
-print(ET.tostring(root))
+# print(ET.tostring(root))
+for actor in root.findall('ACTOR'):
+    print(tree.findall('ACTOR'))
+    print(actor)
+    print('\n')
